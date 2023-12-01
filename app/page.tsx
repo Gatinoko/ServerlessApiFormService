d84ad9162a35c649fb31d7d38607b07d9661edf7
Gabriel Tinoco <gabrielslai@gmail.com>
2023-11-30 22:23:20 -0500

    Cleaned up landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<h1>Main page</h1>
-			<Button>Click me</Button>
 		</main>
 	);
 }
